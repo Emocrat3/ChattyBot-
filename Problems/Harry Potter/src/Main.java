02a3553e256f916b795e4894ea2acacab27ef154
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-05-28 21:20:58 -0400

    Switch statement

---
+++ Problems/Harry Potter/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String character = in.nextLine();
+        switch (character) {
+            case "gryffindor":
+                System.out.println("bravery");
+                break;
+            case "hufflepuff":
+                System.out.println("loyalty");
+                break;
+            case "slytherin":
+                System.out.println("cunning");
+                break;
+            case "ravenclaw":
+                System.out.println("intellect");
+                break;
+            default:
+                System.out.println("not a valid house");
+                break;
+        }
+    }
+}
